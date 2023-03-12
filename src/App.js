bfe8417a2c6f80495971ce953be1faf587dbd318
Ivaylo Hristov <megapr0n94@gmail.com>
2023-03-12 14:27:46 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Zdravko, verno e govno!
+          Welcome to Dev
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          uploaded on development branch
         </a>
       </header>
     </div>
